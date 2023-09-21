b1d78f7f5c44b9c9663c35088f7761a37523b13d
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-09-21 23:44:20 +0530

    Clock Component

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,14 +1,20 @@
 // src/Clock.js
+// importing required modules
 import React, { useState, useEffect } from 'react';
-
-function Clock() {
+//clock arrow function
+const Clock = () => {
+    //use state hook use for state change in hook
     const [time, setTime] = useState(new Date());
 
     useEffect(() => {
+        //setInterval is a global function in JavaScript that takes two arguments: a function to execute and a time interval (in milliseconds).
+        //It repeatedly calls the specified function with the given time interval between each execution until it is explicitly stopped.
+        //intervalId is a unique identifier for the interval, which can later be used to stop the interval using clearInterval.
         const intervalId = setInterval(() => {
             setTime(new Date());
         }, 1000);
-
+        //clearInterval is used to stop a previously started interval created with setInterval. It takes the intervalId returned by setInterval as its argument.
+        //When you call clearInterval(intervalId), it cancels the execution of the function specified in setInterval, effectively stopping the interval.
         return () => clearInterval(intervalId);
     }, []);
 
@@ -20,6 +26,6 @@ function Clock() {
             <p>{formattedTime}</p>
         </div>
     );
-}
+};
 
 export default Clock;
